5e69391e0b669a54284243fa10290305e0a71e80
Hampus <hampusek96@gmail.com>
2019-12-07 12:44:45 +0100

    Done with part 1 day 3

--- day3/sol1.go
+++ day3/sol1.go
@@ -22,15 +22,34 @@ func main() {
 	input1, input2 := splitInputs(content)
 
 	line1Points := calcLinePoints(input1)
-	_ = calcLinePoints(input2)
+	line2Points := calcLinePoints(input2)
 
+	_, distance := findClosestIntersection(line1Points, line2Points)
 
-	fmt.Printf("%v", line1Points)
+	fmt.Printf("%d", distance)
+}
+
+func findClosestIntersection(line1Points, line2Points []Point) (Point,int) {
+	closest := 0
+	var closestPoint Point 
+	for _, point1 := range line1Points {
+		for _, point2 := range line2Points {
+			if point1.x == point2.x && point1.y == point2.y && point1.x != 0 && point1.y != 0 {
+				distance := abs(point1.x) + abs(point1.y)
+				if distance < closest || closest == 0 {
+					closest = distance
+					closestPoint = point1
+				}
+			}
+		}
+	}
+
+	return closestPoint, closest
 }
 
 func splitInputs(input []byte) ([]string, []string) {
 	s := string(input)
-	inputs := strings.Split(s, "\n")
+	inputs := strings.Split(s, "\r\n")
 	input1 := strings.Split(inputs[0], ",")
 	input2 := strings.Split(inputs[1], ",")
 	return input1, input2
@@ -42,20 +61,29 @@ func calcLinePoints(inputs []string) []Point {
 	resultPoints = append(resultPoints, Point{0, 0})
 
 	for _, item := range inputs {
-		newPoint := resultPoints[len(resultPoints)-1] 
 		distance, _ := strconv.Atoi(item[1:])
-		if item[0] == 'U' {
-			newPoint.x += distance
-		} else if item[0] == 'D' {
-			newPoint.x -= distance
-		} else if item[0] == 'L' {
-			newPoint.y -= distance
-		} else if item[0] == 'R' {
-			newPoint.y += distance
-		}
+		for i := 0; i < distance; i++ {
+			newPoint := resultPoints[len(resultPoints)-1]
+			if item[0] == 'U' {
+				newPoint.y++
+			} else if item[0] == 'D' {
+				newPoint.y--
+			} else if item[0] == 'L' {
+				newPoint.x--
+			} else if item[0] == 'R' {
+				newPoint.x++
+			}
 
-		resultPoints = append(resultPoints, newPoint)
+			resultPoints = append(resultPoints, newPoint)
+		}
 	}
 
 	return resultPoints
+}
+
+func abs(num int) int {
+	if num < 0 {
+		return -num
+	}
+	return num
 }
